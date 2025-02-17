7d784c03ed30e2830bbff6c37819de0d436a10e7
Wemi Moyela <wemimoyela@gmail.com>
2025-02-17 14:14:51 +0100

    complete user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,8 +26,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: "User was successfully created." }
-        format.json { render :show, status: :created, location: @user }
+        format.html { redirect_to @user, notice: "You've signed up to Mini-Twitter!" }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @user.errors, status: :unprocessable_entity }
