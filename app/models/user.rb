7667f0973f6c7eedf457b568bc58fffa0f5ad1cb
Wemi Moyela <wemimoyela@gmail.com>
2025-02-17 10:59:15 +0100

    initialize rails and create user model

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+    has_secure_password
+    
+    before_save { email.downcase!, username.downcase! }
+
+    validates :username, presence: true, length: { minimum: 3, maximum: 25 },
+                                         uniqueness: true
+    
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i                  
+    validates :email, presence: true, length: { maximum: 255 },
+                                      format: { with: VALID_EMAIL_REGEX },
+                                      uniqueness: true
+end
